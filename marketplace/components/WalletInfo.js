9a421736aa06ed8bedd9b153531500c977d10189
derek2403 <derekliew0@gmail.com>
2024-08-03 16:07:12 +0800

    update wallet style

--- marketplace/components/WalletInfo.js
+++ marketplace/components/WalletInfo.js
@@ -110,9 +110,9 @@ export default function WalletInfo() {
           padding: 10px;
           position: absolute;
           top: 100%;
-          left: 50%;
+          left: 95%;
           transform: translateX(-50%);
-          width: 220px;
+          width: 360px;
           box-shadow: 0 4px 6px rgba(0, 0, 0, 0.2);
           text-align: left;
         }
